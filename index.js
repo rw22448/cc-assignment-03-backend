41d0b101f757ee5bc51399150a017c7aabed3241
Richard Wang <rw22448@gmail.com>
2021-05-16 20:08:57 +1000

    Edit structure to fix deployment

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
+const serverless = require('serverless-http');
 const express = require('express');
-const cors = require('cors');
-
-require('dotenv').config();
-
 const app = express();
-
-app.use(cors());
-
-const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+module.exports.handler = serverless(app);
