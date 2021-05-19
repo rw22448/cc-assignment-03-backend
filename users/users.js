d6f1e606fb9865482637fbd4cd95fcb837bdef52
Richard Wang <rw22448@gmail.com>
2021-05-19 14:03:34 +1000

    Disallow users from creating account if username already exists

--- users/users.js
+++ users/users.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const aws = require('aws-sdk');
+const axios = require('axios');
 
 const router = express.Router({ mergeParams: true });
 
 const USERS_TABLE = process.env.USERS_TABLE;
+const IS_OFFLINE = process.env.IS_OFFLINE;
 const dynamodb = new aws.DynamoDB.DocumentClient();
 
 router.get('/get-user-by-username/:username', (req, res) => {
@@ -33,7 +35,7 @@ router.get('/get-user-by-username/:username', (req, res) => {
   }
 });
 
-router.post('/create-user', (req, res) => {
+router.post('/create-user', async (req, res) => {
   const { username, password } = req.body;
   if (!(typeof username == 'string' && typeof password == 'string')) {
     res
@@ -49,14 +51,32 @@ router.post('/create-user', (req, res) => {
     },
   };
 
-  dynamodb.put(params, (error) => {
-    if (error) {
+  let baseUrl = IS_OFFLINE
+    ? 'http://' + req.get('host')
+    : 'https://' + req.get('host');
+
+  userExists = await axios
+    .get(`${baseUrl}/users/get-user-by-username/${req.body.username}`)
+    .then((result) => {
+      console.log(result.data);
+      return result.data;
+    })
+    .catch((error) => {
       console.log(error);
-      res.status(400).json({ error: 'Unable to create user' });
-    } else {
-      res.status(200).json({ username });
-    }
-  });
+    });
+
+  if (userExists) {
+    res.status(400).json({ error: `User ${req.body.username} already exists` });
+  } else {
+    dynamodb.put(params, (error) => {
+      if (error) {
+        console.log(error);
+        res.status(400).json({ error: 'Unable to create user' });
+      } else {
+        res.status(200).json({ username });
+      }
+    });
+  }
 });
 
 module.exports = router;
