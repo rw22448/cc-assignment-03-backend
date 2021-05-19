1594f58f3934f4f17a1c826ab095450e2b9e0371
Richard Wang <rw22448@gmail.com>
2021-05-20 00:44:03 +1000

    Add update and delete user endpoints

--- users/users.js
+++ users/users.js
@@ -9,30 +9,32 @@ const IS_OFFLINE = process.env.IS_OFFLINE;
 const dynamodb = new aws.DynamoDB.DocumentClient();
 
 router.get('/get-user-by-username/:username', (req, res) => {
-  if (!req.params.username) {
+  const { username } = req.params;
+
+  if (!username) {
     res.status(400).json({ error: 'Bad request' });
-  } else {
-    const params = {
-      TableName: USERS_TABLE,
-      Key: {
-        username: req.params.username,
-      },
-    };
-
-    dynamodb.get(params, (error, data) => {
-      if (error) {
-        console.log(error, error.stack);
-        res.status(400).json({ error: 'Unable to fetch user' });
-      }
-
-      if (data.Item) {
-        const { username } = data.Item;
-        res.status(200).json({ username });
-      } else {
-        res.status(404).json({ error: 'User not found' });
-      }
-    });
   }
+
+  const params = {
+    TableName: USERS_TABLE,
+    Key: {
+      username: username,
+    },
+  };
+
+  dynamodb.get(params, (error, data) => {
+    if (error) {
+      console.log(error, error.stack);
+      res.status(400).json({ error: 'Unable to fetch user' });
+    }
+
+    if (data.Item) {
+      const { username } = data.Item;
+      res.status(200).json({ username });
+    } else {
+      res.status(404).json({ error: 'User not found' });
+    }
+  });
 });
 
 router.post('/create-user', async (req, res) => {
@@ -56,7 +58,7 @@ router.post('/create-user', async (req, res) => {
     : 'https://' + req.get('host') + '/dev';
 
   userExists = await axios
-    .get(`${baseUrl}/users/get-user-by-username/${req.body.username}`)
+    .get(`${baseUrl}/users/get-user-by-username/${username}`)
     .then((result) => {
       console.log(result.data);
       return result.data;
@@ -66,9 +68,7 @@ router.post('/create-user', async (req, res) => {
     });
 
   if (userExists) {
-    res
-      .status(400)
-      .json({ error: `User '${req.body.username}' already exists` });
+    res.status(409).json({ error: `User '${username}' already exists` });
   } else {
     dynamodb.put(params, (error) => {
       if (error) {
@@ -78,6 +78,95 @@ router.post('/create-user', async (req, res) => {
         res.status(200).json({ username });
       }
     });
+  }
+});
+
+router.put('/update-user-password', async (req, res) => {
+  const { username, password } = req.body;
+  if (!(username && password)) {
+    res.status(400).json({ error: 'Bad request' });
+  }
+
+  const params = {
+    TableName: USERS_TABLE,
+    Key: {
+      username: username,
+    },
+    UpdateExpression: 'set #password = :a',
+    ExpressionAttributeNames: {
+      '#password': 'password',
+    },
+    ExpressionAttributeValues: {
+      ':a': password,
+    },
+  };
+
+  let baseUrl = IS_OFFLINE
+    ? 'http://' + req.get('host')
+    : 'https://' + req.get('host') + '/dev';
+
+  userExists = await axios
+    .get(`${baseUrl}/users/get-user-by-username/${username}`)
+    .then((result) => {
+      console.log(result.data);
+      return result.data;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+  if (userExists) {
+    dynamodb.update(params, (error) => {
+      if (error) {
+        console.log(err);
+        res.status(400).json({ error: 'Unable to update user' });
+      } else {
+        res.status(200).json({ username });
+      }
+    });
+  } else {
+    res.status(404).json({ error: `User '${username}' does not exist` });
+  }
+});
+
+router.delete('/delete-user-by-username/:username', async (req, res) => {
+  const { username } = req.params;
+  if (!username) {
+    res.status(400).json({ error: 'Bad request' });
+  }
+
+  const params = {
+    TableName: USERS_TABLE,
+    Key: {
+      username: username,
+    },
+  };
+
+  let baseUrl = IS_OFFLINE
+    ? 'http://' + req.get('host')
+    : 'https://' + req.get('host') + '/dev';
+
+  userExists = await axios
+    .get(`${baseUrl}/users/get-user-by-username/${username}`)
+    .then((result) => {
+      console.log(result.data);
+      return result.data;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+  if (userExists) {
+    dynamodb.delete(params, (error) => {
+      if (error) {
+        console.log(err);
+        res.status(400).json({ error: 'Unable to delete user' });
+      } else {
+        res.status(200).json({ username });
+      }
+    });
+  } else {
+    res.status(404).json({ error: `User '${username} does not exist` });
   }
 });
 
