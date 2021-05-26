c4fd43b91d8498ba9b1d437504edafa4bdffa6ac
Richard Wang <rw22448@gmail.com>
2021-05-27 00:08:52 +1000

    Add authentication, add /events/add-attendees

--- events/events.js
+++ events/events.js
@@ -7,14 +7,45 @@ const router = express.Router({ mergeParams: true });
 
 const IS_OFFLINE = process.env.IS_OFFLINE;
 const EVENTS_TABLE = process.env.EVENTS_TABLE;
+const ACTIVE_USERS_TABLE = process.env.ACTIVE_USERS_TABLE;
 const dynamodb = new aws.DynamoDB.DocumentClient();
 
-// const isAuthenticated = (req, res, next) => {
-//   console.log('isAuthenticated called');
-//   next();
-// };
+const isAuthenticated = (req, res, next) => {
+  authUser = req.header('cc-authentication-user');
+  authToken = req.header('cc-authentication-token');
 
-// router.use(isAuthenticated);
+  if (!(authUser && authToken)) {
+    res.status(401).json({ error: 'Unauthorized' });
+  } else {
+    const params = {
+      TableName: ACTIVE_USERS_TABLE,
+      Key: {
+        username: authUser,
+      },
+    };
+
+    dynamodb.get(params, (error, data) => {
+      if (error) {
+        console.log(error);
+        res.status(400).json({ error: 'Error' });
+      }
+
+      if (data.Item) {
+        const { token } = data.Item;
+
+        if (token != authToken) {
+          res.status(401).json({ error: 'Unauthorized' });
+        } else {
+          next();
+        }
+      } else {
+        res.status(401).json({ error: 'Unauthorized' });
+      }
+    });
+  }
+};
+
+router.use(isAuthenticated);
 
 router.get('/', (req, res) => {
   res.status(200).json({ message: 'Success' });
@@ -36,9 +67,7 @@ router.post('/create-event', (req, res) => {
         title: title,
         description: description,
         creator: creator,
-        attendees: {
-          SS: attendees,
-        },
+        attendees: attendees,
         startTime: startTime,
         endTime: endTime,
       },
@@ -125,8 +154,15 @@ router.delete('/delete-event-by-id/:id', async (req, res) => {
       ? 'http://' + req.get('host')
       : 'https://' + req.get('host') + '/dev';
 
+    const headers = {
+      headers: {
+        'cc-authentication-user': req.header('cc-authentication-user'),
+        'cc-authentication-token': req.header('cc-authentication-token'),
+      },
+    };
+
     const event = await axios
-      .get(`${baseUrl}/events/get-event-by-id/${id}`)
+      .get(`${baseUrl}/events/get-event-by-id/${id}`, headers)
       .then((result) => {
         console.log(result.data);
         return result.data;
@@ -160,8 +196,15 @@ router.put('/update-event', async (req, res) => {
       ? 'http://' + req.get('host')
       : 'https://' + req.get('host') + '/dev';
 
+    const headers = {
+      headers: {
+        'cc-authentication-user': req.header('cc-authentication-user'),
+        'cc-authentication-token': req.header('cc-authentication-token'),
+      },
+    };
+
     const event = await axios
-      .get(`${baseUrl}/events/get-event-by-id/${id}`)
+      .get(`${baseUrl}/events/get-event-by-id/${id}`, headers)
       .then((result) => {
         console.log(result.data);
         return result.data;
@@ -203,6 +246,66 @@ router.put('/update-event', async (req, res) => {
     } else {
       res.status(404).json({ error: `Event does not exist` });
     }
+  }
+});
+
+router.put('/add-attendees', async (req, res) => {
+  const { id, attendees } = req.body;
+
+  if (Array.isArray(attendees) && id) {
+    let baseUrl = IS_OFFLINE
+      ? 'http://' + req.get('host')
+      : 'https://' + req.get('host') + '/dev';
+
+    const headers = {
+      headers: {
+        'cc-authentication-user': req.header('cc-authentication-user'),
+        'cc-authentication-token': req.header('cc-authentication-token'),
+      },
+    };
+
+    const event = await axios
+      .get(`${baseUrl}/events/get-event-by-id/${id}`, headers)
+      .then((result) => {
+        console.log(result.data);
+        return result.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    if (event) {
+      let updatedAttendees = event.attendees;
+      attendees.forEach((attendant) => {
+        updatedAttendees.push(attendant);
+      });
+
+      const params = {
+        TableName: EVENTS_TABLE,
+        Key: {
+          id: event.id,
+        },
+        UpdateExpression: 'set #attendees = :a',
+        ExpressionAttributeNames: {
+          '#attendees': 'attendees',
+        },
+        ExpressionAttributeValues: {
+          ':a': updatedAttendees,
+        },
+      };
+      dynamodb.update(params, (error) => {
+        if (error) {
+          console.log(err);
+          res.status(400).json({ error: 'Unable to update event' });
+        } else {
+          res.status(200).json({ id: event.id, attendees: updatedAttendees });
+        }
+      });
+    } else {
+      res.status(404).json({ error: `Event does not exist` });
+    }
+  } else {
+    res.status(400).json({ error: 'Bad request' });
   }
 });
 
