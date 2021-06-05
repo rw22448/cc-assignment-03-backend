311f197c3754789c60a49525ce7321e8c29a17b8
Richard Wang <rw22448@gmail.com>
2021-06-05 17:50:29 +1000

    Add CORS

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const serverless = require('serverless-http');
 const express = require('express');
+const cors = require('cors');
 
 const users = require('./users/users.js');
 const events = require('./events/events.js');
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json({ strict: false, limit: '2000kb' }));
 
